6773ce4c8639df5a07923bf82a4dc1036fbefa37
Анастасія Олійник <Anastasiia.Oliinyk.PM.2022@edu.lpnu.ua>
2023-11-21 19:17:19 +0200

    comit 7

--- app/js/code.js
+++ app/js/code.js
@@ -1,87 +1,67 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const btns = document.querySelectorAll('.btn-toggle');
-  const pageContent = document.getElementById('page-content');
-  const pageContent2 = document.getElementById('page-content2');
+    const btns = document.querySelectorAll('.btn-toggle');
+    const pageContents = {
+        'btn1': document.getElementById('page-content'),
+        'btn2': document.getElementById('page-content2')
+    };
+    let selectedButton = null;
 
-  btns.forEach(btn => {
-      btn.addEventListener('click', function () {
-          // Перевірка, чи кнопка вже активна
-          const isActive = btn.classList.contains('active1');
+    btns.forEach(btn => {
+        btn.addEventListener('click', function () {
+            // Перевірка, чи кнопка вже активна
+            const isActive = btn.classList.contains('active1');
 
-          // Сховування контенту при натисканні будь-якої іншої кнопки
-          if (btn !== btn1) {
-              pageContent.style.display = "none";
-          }
+            // Приховування контенту попередньої кнопки
+            if (selectedButton !== null && !isActive) {
+                const previousContent = pageContents[selectedButton.id];
+                if (previousContent) {
+                    previousContent.style.display = "none";
+                }
+            }
 
-          if (btn !== btn2) {
-              pageContent2.style.display = "none";
-          }
+            // Видалення класу active1 з усіх кнопок
+            btns.forEach(otherBtn => {
+                otherBtn.classList.remove('active1');
+            });
 
-          // Видалення класу active1 з усіх кнопок
-          btns.forEach(otherBtn => {
-              otherBtn.classList.remove('active1');
+            // Відображення відповідного контенту
+            const targetContent = pageContents[btn.id];
+            if (targetContent) {
+                targetContent.style.display = "block";
+            }
 
-          });
+            // Додавання/видалення класу active1 на поточній кнопці
+            btn.classList.toggle('active1');
 
-          // Додавання класу active1 на поточну кнопку, якщо вона не була активною
-          if (!isActive) {
-              btn.classList.add('active1');
-
-          }
-
-          // Відображення відповідного контенту
-          if (btn === btn1) {
-              pageContent.style.display = "block";
-          } else if (btn === btn2) {
-              pageContent2.style.display = "block";
-          }
-      });
-  });
-});
-
-const btn1 = document.getElementById("btn1");
-const rectangle = document.querySelector(".rectangle1");
-
-btn1.addEventListener("click", function() {
-  // Спочатку перевіряємо, чи прямокутник видимий
-  if (rectangle.style.display === "none" || rectangle.style.display === "") {
-    // Якщо прямокутник прихований, показуємо його
-    rectangle.style.display = "block";
-  } else {
-    // Якщо прямокутник видимий, залишаємо його видимим
-    rectangle.style.display = "block";
-  }
-});
-
-const btn2 = document.getElementById("btn2"); // Додаємо іншу кнопку
-
-btn2.addEventListener("click", function() {
-  // При натисканні на іншу кнопку (btn2), ховаємо прямокутник
-  rectangle.style.display = "none";
-});
-
-
-const btns = document.querySelectorAll('.btn-toggle');
-
-btns.forEach(btn => {
-  btn.addEventListener('click', function () {
-    // Видалення класу 'active1' з усіх кнопок
-    btns.forEach(otherBtn => {
-      otherBtn.classList.remove('active1');
+            // Оновлення вибраної кнопки
+            selectedButton = btn;
+        });
     });
-
-    // Додавання класу 'active1' на поточну кнопку
-    btn.classList.add('active1');
-    
-   
-     
-  });
 });
 
 
 
 
-  
-  
-  
-  
+
+/*function changeClass(index) {
+    const buttons = document.querySelectorAll('.btn-toggle');
+    const rectangle = document.querySelector('.rectangle1');
+
+    rectangle.classList.toggle('visible');
+
+    if (selectedButton !== null) {
+        selectedButton.classList.remove('active1', 'rectangle1');
+        const previousContent = pageContents[selectedButton.id];
+        if (previousContent) {
+            previousContent.style.display = "none";
+        }
+    }
+
+    buttons[index].classList.toggle('active1', 'rectangle1');
+    const targetContent = pageContents[buttons[index].id];
+    if (targetContent) {
+        targetContent.style.display = "block";
+    }
+
+    selectedButton = buttons[index];
+}*/
