b1e633b9a458eda3e876be3a3f5e831486b8fc6a
Анастасія Олійник <Anastasiia.Oliinyk.PM.2022@edu.lpnu.ua>
2023-11-27 18:51:42 +0200

    comit7

--- app/js/code.js
+++ app/js/code.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const btns = document.querySelectorAll('.btn-toggle');
     const pageContents = {
         'btn1': document.getElementById('page-content'),
-        'btn2': document.getElementById('page-content2')
     };
+
     let selectedButton = null;
 
     btns.forEach(btn => {
@@ -38,30 +38,161 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+var var1 = localStorage.getItem('savedVar1') || 1234;
 
+// Функція для виведення alert та оновлення текстового вмісту при натисканні
+function showAlert() {
+  alert(parseInt(var1) + 1);
+  var1 = parseInt(var1) + 1;
 
+  // Збереження нового значення у localStorage
+  localStorage.setItem('savedVar1', var1);
 
+  // Оновлення текстового вмісту елементу з класом "item-views panel"
+  document.querySelector('.item-views.panel.panel1').textContent = var1;
+}
 
+// Встановлення початкового значення при завантаженні сторінки
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.item-views.panel.panel1').textContent = var1;
+});
 
-/*function changeClass(index) {
-    const buttons = document.querySelectorAll('.btn-toggle');
-    const rectangle = document.querySelector('.rectangle1');
+var var2 = localStorage.getItem('savedVar2') || 987;
+// Функція для виведення alert та оновлення текстового вмісту при натисканні
+function showAlertFromButton() {
+  alert(parseInt(var2) + 1);
+  var2 = parseInt(var2) + 1;
 
-    rectangle.classList.toggle('visible');
+  // Збереження нового значення у localStorage
+  localStorage.setItem('savedVar2', var2);
 
-    if (selectedButton !== null) {
-        selectedButton.classList.remove('active1', 'rectangle1');
-        const previousContent = pageContents[selectedButton.id];
-        if (previousContent) {
-            previousContent.style.display = "none";
+  // Оновлення текстового вмісту елементу з класом "item-views panel"
+  document.querySelector('.item-views.panel.panel2').textContent = var2;
+}
+
+// Встановлення початкового значення при завантаженні сторінки
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.item-views.panel.panel2').textContent = var2;
+});
+
+var var3 = localStorage.getItem('savedVar3') || 543;
+// Функція для виведення alert та оновлення текстового вмісту при натисканні
+function showAlertFromButton1() {
+  alert(parseInt(var3) + 1);
+  var3 = parseInt(var3) + 1;
+
+  // Збереження нового значення у localStorage
+  localStorage.setItem('savedVar3', var3);
+
+  // Оновлення текстового вмісту елементу з класом "item-views panel"
+  document.querySelector('.item-views.panel.panel3').textContent = var3;
+}
+
+// Встановлення початкового значення при завантаженні сторінки
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.item-views.panel.panel3').textContent = var3;
+});
+function showMessage() {
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+        footer: '<a href="#">Why do I have this issue?</a>'
+      });
+  }
+  document.addEventListener('DOMContentLoaded', function () {
+    renderCalendar();
+
+    const monthSelector = document.getElementById('monthSelector');
+    monthSelector.addEventListener('change', function () {
+      currentMonth = parseInt(monthSelector.value);
+      renderCalendar();
+    });
+  });
+
+  let currentMonth = new Date().getMonth();
+  let currentYear = new Date().getFullYear();
+  let useBorder = true;
+  let selectedDay = null;
+
+  function renderCalendar() {
+    const calendarElement = document.getElementById('calendar');
+    const currentDate = new Date();
+    const firstDayOfMonth = new Date(currentYear, currentMonth, 1);
+    const lastDayOfMonth = new Date(currentYear, currentMonth + 1, 0);
+
+    const daysInMonth = lastDayOfMonth.getDate();
+    const startingDay = (firstDayOfMonth.getDay() + 6) % 7; // Починаємо з понеділка
+
+    const monthNames = [
+      'Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень',
+      'Липень', 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень'
+    ];
+
+    const table = document.createElement('table');
+    const header = table.createTHead();
+
+    const monthYearRow = header.insertRow(0);
+   const monthYearCell = monthYearRow.insertCell(0);
+    monthYearCell.colSpan = 7;
+    monthYearCell.classList.add('month-year');
+
+    const daysOfWeekRow = header.insertRow(1);
+
+    for (let i = 1; i < 8; i++) {
+      const cell = daysOfWeekRow.insertCell(i - 1);
+      cell.innerHTML = ['S', 'M','T', 'W', 'T', 'F', 'S'][i % 7];
+    }
+
+    const body = table.createTBody();
+    let day = 1;
+
+    for (let i = 0; i < 6; i++) {
+      const row = body.insertRow(i);
+
+      for (let j = 0; j < 7; j++) {
+        if (i === 0 && j < startingDay) {
+          const cell = row.insertCell(j);
+          cell.innerHTML = '';
+        } else if (day > daysInMonth) {
+          break;
+        } else {
+          const cell = row.insertCell(j);
+          const dayButton = document.createElement('div');
+          dayButton.classList.add('day-button');
+          dayButton.textContent = day;
+
+          if ([1, 6, 13, 27].includes(day)) {
+            dayButton.classList.add('current-month-day');
+          }
+
+          if (useBorder && day === currentDate.getDate() && currentMonth === currentDate.getMonth() && currentYear === currentDate.getFullYear()) {
+            dayButton.classList.add('today');
+          }
+
+          if (useBorder && selectedDay === day) {
+            dayButton.classList.add('bordered');
+          }
+
+          if (!useBorder && selectedDay === day) {
+            dayButton.classList.add('filled');
+          }
+
+         
+
+          cell.appendChild(dayButton);
+          day++;
         }
+      }
     }
 
-    buttons[index].classList.toggle('active1', 'rectangle1');
-    const targetContent = pageContents[buttons[index].id];
-    if (targetContent) {
-        targetContent.style.display = "block";
-    }
+    calendarElement.innerHTML = '';
+    calendarElement.appendChild(table);
+  }
 
-    selectedButton = buttons[index];
-}*/
+  function toggleFunction() {
+    useBorder = !useBorder;
+    renderCalendar();
+  }
+
+ 
